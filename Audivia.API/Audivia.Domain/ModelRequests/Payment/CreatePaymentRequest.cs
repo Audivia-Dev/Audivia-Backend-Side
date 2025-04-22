68f8c0a32bc2a102a82551750fec601991941566
PhuocLoc <135364306+PhuocLoc3012@users.noreply.github.com>
2025-04-22 22:19:47 +0700

    init class

---
+++ Audivia.API/Audivia.Domain/ModelRequests/Payment/CreatePaymentRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Audivia.Domain.ModelRequests.Payment
+{
+    public class CreatePaymentRequest
+    {
+        public string OrderCode { get; set; }
+        public string ReturnUrl { get; set; }
+        public string CancelUrl { get; set; }
+        public long Amount { get; set; }
+        public string Description { get; set; }
+        public string Signature { get; set; }
+
+    }
+}
