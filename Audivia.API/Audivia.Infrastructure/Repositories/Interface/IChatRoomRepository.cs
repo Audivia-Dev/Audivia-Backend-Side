9889b04908817605bdf98e504bd6555ff5fbebb2
PhuocLoc <135364306+PhuocLoc3012@users.noreply.github.com>
2025-05-16 08:56:39 +0700

    update api get chatroom by id

--- Audivia.API/Audivia.Infrastructure/Repositories/Interface/IChatRoomRepository.cs
+++ Audivia.API/Audivia.Infrastructure/Repositories/Interface/IChatRoomRepository.cs
@@ -6,5 +6,6 @@ namespace Audivia.Infrastructure.Repositories.Interface
     public interface IChatRoomRepository : IBaseRepository<ChatRoom>, IDisposable
     {
         Task<List<ChatRoom>> GetChatRoomsOfUser(string userId);
+        Task<ChatRoom> GetChatRoomById(string id);
     }
 }
