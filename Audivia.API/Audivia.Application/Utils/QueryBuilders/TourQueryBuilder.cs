02b5fd7ba296e57d3f0a6c1846ec1e9d644f7c11
Vo Thi Mai Hoa <vothimaihoa123@gmail.com>
2025-06-12 10:30:55 +0700

    [UPDATE] search by tour title

--- Audivia.API/Audivia.Application/Utils/QueryBuilders/TourQueryBuilder.cs
+++ Audivia.API/Audivia.Application/Utils/QueryBuilders/TourQueryBuilder.cs
@@ -12,6 +12,14 @@ namespace Audivia.Application.Utils.QueryBuilders
             var filterBuilder = Builders<Tour>.Filter;
             var filter = filterBuilder.Empty;
 
+            if (!string.IsNullOrEmpty(request.Title))
+            {
+                var regex = new BsonRegularExpression(request.Title, "i");
+                var notNullFilter = filterBuilder.Ne(t => t.Title, null);
+                var nameFilter = filterBuilder.Regex("title", regex);
+                filter &= filterBuilder.And(notNullFilter, nameFilter);
+            }
+
             if (!string.IsNullOrEmpty(request.TourTypeId))
             {
                 filter &= filterBuilder.Eq(t => t.TypeId, request.TourTypeId);
